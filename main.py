cb3bcc8eddcb6bde6093f2743be9c4103a845539
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-04-25 17:45:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,19 +24,6 @@ mysql = MySQL(app)
 def hello():
 	return "Hello World"
 
-@app.route("/login/predict", methods=["GET"])
-@auth.login_required
-def predict():
-    house_form = HouseForm()
-    if house_form.validate_on_submit():
-        Avg_Area_Income = float(house_form.Avg_Area_Income.data)
-        Avg_Area_House_Age = float(house_form.Avg_Area_House_Age.data)
-        Avg_Area_Number_of_Rooms = float(house_form.Avg_Area_Number_of_Rooms.data)
-        Avg_Area_Number_of_Bedrooms = float(house_form.Avg_Area_Number_of_Bedrooms.data)
-        Area_Population = float(house_form.Area_Population.data)
-    return render_template("car.html", form=house_form)
-
-
 @auth.verify_password
 def authenticate(username, password):
     if username and password:
@@ -62,80 +49,54 @@ def authenticate(username, password):
     return False
 
 @app.route("/login/predict", methods=["POST"])
+@auth.login_required
 def result():
     try:
-        form = request.form
+        Avg_Area_Income = request.args['Avg_Area_Income']
+        Avg_Area_House_Age = request.args['Avg_Area_House_Age']
+        Avg_Area_Number_of_Rooms = request.args['Avg_Area_Number_of_Rooms']
+        Avg_Area_Number_of_Bedrooms = request.args['Avg_Area_Number_of_Bedrooms']
+        Area_Population = request.args['Area_Population']
+        print(Avg_Area_Income,Avg_Area_Number_of_Rooms)
         model = joblib.load("aiml/house.pkl")
-        new_house = np.array(
-            [float(form['Avg_Area_Income']), float(form['Avg_Area_House_Age']),
-             float(form['Avg_Area_Number_of_Rooms']), float(form['Avg_Area_Number_of_Bedrooms']), float(form['Area_Population'])
-            ]).reshape(1, -1)
+        new_house = np.array([Avg_Area_Income, Avg_Area_House_Age,Avg_Area_Number_of_Rooms,Avg_Area_Number_of_Bedrooms,Area_Population]).reshape(1, -1)
+        print(new_house)
         predicted_price = model.predict(new_house)
+        print(predicted_price)
         if predicted_price < 0:
-            predicted_price = 0
-        return jsonify(f"The Price of house is {float(predicted_price)}")
+            predicted_price = -(predicted_price)
+        return jsonify(f"{float(predicted_price)}")
     except ValueError:
         return jsonify(f"{ValueError}")
 
-@app.route('/login', methods =['GET', 'POST'])
-def login():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM login WHERE username = % s AND password = % s', (username, password, ))
-		account = cursor.fetchone()
-		if account:
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
-			add_app = ("INSERT into pricing "
-					   "(username)"
-					   "VALUES (%(username)s )")
-			data_app = {
-				'username': account['username']
-			}
-			cursor.execute(add_app, data_app)
-			mysql.connection.commit()
-			return redirect(url_for('predict'))
-		else:
-			msg = 'Incorrect username / password !'
-	return render_template('login.html', msg = msg)
-
-@app.route('/logout')
-def logout():
-	session.pop('loggedin', None)
-	session.pop('id', None)
-	session.pop('username', None)
-	return redirect(url_for('login'))
-
 @app.route('/register', methods =['GET', 'POST'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM login WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		elif not username or not password or not email:
-			msg = 'Please fill out the form !'
-		else:
-			cursor.execute('INSERT INTO login VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+    msg = ''
+    username= request.args['username']
+    password= request.args['password']
+    email= request.args['email']
+    if request.method == 'POST' and username and password and email :
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM login WHERE username = % s AND password = % s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+          msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+          msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+          msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+          msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO login VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+      msg = 'Please fill out the form !'
+    return jsonify(f"{msg}")
+
+
 
 if __name__ == '__main__':
   app.run(host = "0.0.0.0",port=7003,debug = True)#,ssl_context='adhoc')
+
