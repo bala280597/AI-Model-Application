394d5f3e2260002aa0a6dc9527c3796b8592b6c0
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-04-29 11:37:50 +0530

    Model/train (#1)
    * Create train.py
    
    * Update train.py
    
    * Create requirements.txt

---
+++ train/train.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import gcsfs
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from gcloud import storage
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../auth.json'
+
+def load_joblib(bucket_name, file_name):
+    fs = gcsfs.GCSFileSystem()
+    with fs.open(f'{bucket_name}/{file_name}') as f:
+        return pd.read_csv(f)
+df = load_joblib('aiml-dataset', 'USA_Housing.csv')
+x = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+       'Avg. Area Number of Bedrooms', 'Area Population']]
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.4,random_state=101)
+lm = LinearRegression()
+lm = lm.fit(X_train, y_train)
+joblib.dump(lm,"house.pkl")
+client = storage.Client()
+bucket = client.get_bucket('aiml_bala')
+blob = bucket.blob('model.pkl')
+blob.upload_from_filename('model.pkl')
